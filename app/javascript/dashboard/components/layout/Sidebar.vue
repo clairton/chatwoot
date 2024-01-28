7ab1a794fc3dbdfc8ee7c78db912d0fab772d2d2
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2024-01-27 23:14:21 -0300

    feature hide contacts for agent

--- app/javascript/dashboard/components/layout/Sidebar.vue
+++ app/javascript/dashboard/components/layout/Sidebar.vue
@@ -93,6 +93,7 @@ export default {
       isOnChatwootCloud: 'globalConfig/isOnChatwootCloud',
       labels: 'labels/getLabelsOnSidebar',
       teams: 'teams/getMyTeams',
+      currentRole: 'getCurrentRole',
     }),
     activeCustomView() {
       if (this.activePrimaryMenu.key === 'contacts') {
@@ -127,11 +128,13 @@ export default {
       );
       const menuItems = this.sideMenuConfig.primaryMenu;
       return menuItems.filter(menuItem => {
+        if (menuItem.key === 'contacts' && this.hideContactsForAgents) {
+          return false;
+        }
         const isAvailableForTheUser = hasPermissions(
           routesWithPermissions[menuItem.toStateName],
           userPermissions
         );
-
         if (!isAvailableForTheUser) {
           return false;
         }
@@ -177,6 +180,14 @@ export default {
       // if it is explicitly stated to show and it has secondary menu items to show
       // showSecondarySidebar corresponds to the UI settings, indicating if the user has toggled it
       return this.showSecondarySidebar && this.hasSecondaryMenu;
+    },
+    hideContactsForAgents() {
+      return (
+        this.isFeatureEnabledonAccount(
+          this.accountId,
+          'hide_contacts_for_agent'
+        ) && this.currentRole !== 'administrator'
+      );
     },
   },
 
