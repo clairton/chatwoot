282840376c0842473e183f346fc8b37f4396dc55
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2024-01-31 09:15:33 -0300

    better erro message on validate whatsapp config

--- app/javascript/dashboard/routes/dashboard/settings/inbox/channels/CloudWhatsapp.vue
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/channels/CloudWhatsapp.vue
@@ -69,7 +69,7 @@ export default {
         });
       } catch (error) {
         useAlert(
-          error.message || this.$t('INBOX_MGMT.ADD.WHATSAPP.API.ERROR_MESSAGE')
+          error.message || this.$t('INBOX_MGMT.ADD.WHATSAPP.API.ERROR_MESSAGE') + '\n' + error
         );
       }
     },
