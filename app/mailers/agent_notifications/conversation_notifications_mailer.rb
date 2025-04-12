4912312d9caf4724c58aa1928a9090d1261ee78a
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2025-04-12 10:54:03 -0300

    feat: translate agent notifications mailer (#679)

--- app/mailers/agent_notifications/conversation_notifications_mailer.rb
+++ app/mailers/agent_notifications/conversation_notifications_mailer.rb
@@ -5,7 +5,10 @@ class AgentNotifications::ConversationNotificationsMailer < ApplicationMailer
     @agent = agent
     @conversation = conversation
     inbox_name = @conversation.inbox&.sanitized_name
-    subject = "#{@agent.available_name}, A new conversation [ID - #{@conversation.display_id}] has been created in #{inbox_name}."
+    subject = default_i18n_subject(
+      agent_name: @agent.available_name, conversation_id: @conversation.display_id,
+      inbox_name:, actor: t('.actor'), happened: t('.happened')
+    )
     @action_url = app_account_conversation_url(account_id: @conversation.account_id, id: @conversation.display_id)
     send_mail_with_liquid(to: @agent.email, subject: subject) and return
   end
@@ -15,7 +18,9 @@ class AgentNotifications::ConversationNotificationsMailer < ApplicationMailer
 
     @agent = agent
     @conversation = conversation
-    subject = "#{@agent.available_name}, A new conversation [ID - #{@conversation.display_id}] has been assigned to you."
+    subject = default_i18n_subject(
+      agent_name: @agent.available_name, conversation_id: @conversation.display_id, inbox_name:
+    )
     @action_url = app_account_conversation_url(account_id: @conversation.account_id, id: @conversation.display_id)
     send_mail_with_liquid(to: @agent.email, subject: subject) and return
   end
@@ -26,7 +31,9 @@ class AgentNotifications::ConversationNotificationsMailer < ApplicationMailer
     @agent = agent
     @conversation = conversation
     @message = message
-    subject = "#{@agent.available_name}, You have been mentioned in conversation [ID - #{@conversation.display_id}]"
+    subject = default_i18n_subject(
+      agent_name: @agent.available_name, conversation_id: @conversation.display_id, inbox_name:
+    )
     @action_url = app_account_conversation_url(account_id: @conversation.account_id, id: @conversation.display_id)
     send_mail_with_liquid(to: @agent.email, subject: subject) and return
   end
@@ -38,7 +45,11 @@ class AgentNotifications::ConversationNotificationsMailer < ApplicationMailer
 
     @agent = agent
     @conversation = conversation
-    subject = "#{@agent.available_name}, New message in your assigned conversation [ID - #{@conversation.display_id}]."
+    subject = default_i18n_subject(
+      agent_name: @agent.available_name,
+      conversation_id: @conversation.display_id,
+      inbox_name: @conversation.inbox&.name
+    )
     @action_url = app_account_conversation_url(account_id: @conversation.account_id, id: @conversation.display_id)
     send_mail_with_liquid(to: @agent.email, subject: subject) and return
   end
@@ -50,7 +61,11 @@ class AgentNotifications::ConversationNotificationsMailer < ApplicationMailer
 
     @agent = agent
     @conversation = conversation
-    subject = "#{@agent.available_name}, New message in your participating conversation [ID - #{@conversation.display_id}]."
+    subject = default_i18n_subject(
+      agent_name: @agent.available_name,
+      conversation_id: @conversation.display_id,
+      inbox_name: @conversation.inbox&.name
+    )
     @action_url = app_account_conversation_url(account_id: @conversation.account_id, id: @conversation.display_id)
     send_mail_with_liquid(to: @agent.email, subject: subject) and return
   end
